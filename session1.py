f65b5bee8481701d542164257f7f5aa36317b396
ritikakhokhar <khokharritika73@gmail.com>
2024-05-27 19:40:47 +1000

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,15 @@
+# write a program to calculate the number if minis in the maxis
+#total maxis is 2.5*minis
+# 1 minis is 5000
+# calculate how much is 4.5 maxis
+
+value for minis is 5000
+maxis is 2.5*minis
+so 4.5 maxis is equal to 2,5*4.5*minis
+display maxis
+
+minis = 5000
+maxis = 2.5*minis
+print("the value for maxis is: ", 4.5*maxis)
+
+
