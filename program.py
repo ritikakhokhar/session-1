c727c68f9a8be37854431a121e21a706e70c8a6a
ritikakhokhar <khokharritika73@gmail.com>
2024-05-27 18:42:00 +1000

    Create-program.py
    apple pie code

---
+++ program.py
@@ -0,0 +1,22 @@
+# Write a program to inpit age of a user and let them know if they have diabete
+# risk or not. Use case is that a user with age more than 65 is risky else not
+# risky
+
+Age = 55
+if age >= 65
+print("This is risky")
+else 
+print("Not risky")
+
+# Write a program to calculate the GST tax of two products, apple , and pie
+# apple has 10% GST and pie has only 8%
+# calculate total GST that need to be deducted from the bill
+# apple  gross sale is $75, pie gross is $100
+
+apple = 75
+pie = 100
+gst_apple = 0.1*apple
+gst_pie = 0'08*pie
+
+print(gst_apple)
+print(gst_pie)
